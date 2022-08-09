66705e142b2318d584c03f74e2b703f0ca8ce2d0
Kristoffer Roberts <kristoffer.roberts@outlook.com>
2022-08-09 04:47:04 -0400

    Update Reverse Cipher.py

--- Reverse Cipher.py
+++ Reverse Cipher.py
@@ -1,13 +1,10 @@
 # Reverse Cipher 
 # https://www.nostarch.com/crackingcodes/ (BSD Licensed)
 
-message = "Three can keep a secret, if two of them are dead."
+message = input("Enter message:")
 translated = ""
 i = len(message) - 1
 while i >= 0:
     translated = translated + message[i]
     i = i -1
-
 print(translated)
-
-
